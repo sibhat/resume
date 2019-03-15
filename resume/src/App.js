4690ea6d1dda1b0a3eac0e7fbb2a034620175f62
sibhat <sibhat.temelso@gmail.com>
2019-03-15 09:57:20 -0700

    added three projects

--- resume/src/App.js
+++ resume/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import Nav from "./features/nav/container/Nav";
 import HeaderContainer from "./features/header/container/HeaderContainer";
 import "./sass/main.scss";
@@ -6,23 +6,22 @@ import ExperienceContainer from "./features/experience/container/Experience";
 import ProjectContainer from "./features/projects/container/ProjectsContainer";
 import EDitSkills from "./features/skills/container/EditSkills";
 import ContactUs from "./features/contact/container/ContactUs";
+
 // import firebase from "./firebaseConfig";
 
 class App extends Component {
-	render() {
-		return (
-			<div className="icontainer">
-				<Nav />
-				<main className="main">
-					<HeaderContainer />
-					<ExperienceContainer />
-					<ProjectContainer />
-					<EDitSkills />
-					<ContactUs />
-				</main>
-			</div>
-		);
-	}
+    render() {
+        return< div className={"icontainer"}>
+                < Nav/>
+                < main className="main">
+                    < HeaderContainer/>
+                    < ExperienceContainer/>
+                    < ProjectContainer/>
+                    < EDitSkills/>
+                    < ContactUs/>
+                </main>
+            </div>
+    }
 }
 
 export default App;
