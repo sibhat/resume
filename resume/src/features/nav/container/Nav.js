14986219509dbc7f747bc4420c3317b98adfa4b7
sibhat <sibhat.temelso@gmail.com>
2019-05-01 19:30:55 -0700

    Nav Container now reads from firebase db

--- resume/src/features/nav/container/Nav.js
+++ resume/src/features/nav/container/Nav.js
@@ -1,56 +1,69 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
+import firebase from "../../../firebaseConfig";
+
 class Nav extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {};
-	}
-	render() {
-		return (
-			<React.Fragment>
-				<nav className="inav">
-					<div className="inav__name">
-						<h2 className="heading__secondary teal-text">
-							Sibhat Temelso
-						</h2>
-						<h4 className="heading__teritary brown-text">
-							Full Stack Developer
-						</h4>
-					</div>
-					<div className="inav__item">
-						<li className="inav__list">
-							<i className="fas fa-user" />
-							<a href="#about" className="waves-effect">
-								About
-							</a>
-						</li>
-						<li className="inav__list">
-							<i className="fas fa-laptop" />
-							<a href="#experience">Experience</a>
-						</li>
-						<li className="inav__list">
-							<i className="fa fa-code" />
-							<a href="#projects">Projects</a>
-						</li>
-						<li className="inav__list">
-							<i className="fa fa-chess-knight" />
-							<a href="#skills">Skills</a>
-						</li>
-						<li className="inav__list">
-							<i className="fas fa-paper-plane" />
-							<a href="#contact">Contact</a>
-						</li>
-						<li className="inav__list">
-							<i className="fas fa-file-code" />
-							<a href="#resume">Resume</a>
-						</li>
-					</div>
-				</nav>
-				<nav className="nav__humberger">
-					<span className="nav__humberger__line" />
-				</nav>
-			</React.Fragment>
-		);
-	}
+    constructor(props) {
+        super(props);
+        this.state = {
+            about: ""
+        }
+    }
+
+    componentDidMount() {
+        let aboutRef = firebase.database().ref('about');
+        aboutRef.on('value', snapshot => {
+            // console.log(snapshot.val());
+            this.setState({about: snapshot.val()})
+        });
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                <nav className="inav">
+                    <div className="inav__name">
+                        <h2 className="heading__secondary teal-text">
+                            {`${this.state.about.firstName}  ${this.state.about.lastName}`}
+                        </h2>
+                        <h4 className="heading__teritary brown-text">
+                            {`${this.state.about.headline}`}
+                        </h4>
+                    </div>
+                    <div className="inav__item">
+                        <li className="inav__list">
+                            <i className="fas fa-user"/>
+                            <a href={"#about"} className="waves-effect">
+                                About
+                            </a>
+                        </li>
+                        <li className="inav__list">
+                            <i className="fas fa-laptop"/>
+                            <a href={"#experience"}>Experience</a>
+                        </li>
+                        <li className="inav__list">
+                            <i className="fa fa-code"/>
+                            <a href={"#projects"}>Projects</a>
+                        </li>
+                        <li className="inav__list">
+                            <i className="fa fa-chess-knight"/>
+                            <a href={"#skills"}>Skills</a>
+                        </li>
+                        <li className="inav__list">
+                            <i className="fas fa-paper-plane"/>
+                            <a href={"#contact"}>Contact</a>
+                        </li>
+                        <li className="inav__list">
+                            <i className="fas fa-file-code"/>
+                            <a href={"#resume"}>Resume</a>
+                        </li>
+                    </div>
+                </nav>
+                <nav className="nav__humberger">
+                    <span className="nav__humberger__line"/>
+                </nav>
+            </React.Fragment>
+        );
+    }
 }
 
 export default Nav;
