edc5463ef08ab02a00c81f3c24d4a7754d2e0baf
sibhat <sibhat.temelso@gmail.com>
2019-05-01 19:32:23 -0700

    Project Container now reads from firebase db

--- resume/src/features/projects/presentational/Project.js
+++ resume/src/features/projects/presentational/Project.js
@@ -1,10 +1,10 @@
 import React from "react";
 
 const Project = props => {
-	let cards = props.projects.map(project => (
-		<div className="card" key={project.title}>
+	let cards = props.projects.map((project, index) => (
+		<div className="card" key={index}>
 			<div className="card__photo">
-				<img src={project.img1} alt="seekgeek" />
+				<img src={project.images[0]} alt="seekgeek" />
 			</div>
 			<div className="card__body">
 				<div className="card__body--header">
@@ -22,6 +22,12 @@ const Project = props => {
 				<div className="card__body--info">
 					<p className="paragaph">{project.description}</p>
 				</div>
+				<ul className="card__list">
+					Stack:
+					{/*{project.stack.map((item, index) => (*/}
+						<li >{project.stack.join(", ")} </li>
+					{/*))}*/}
+				</ul>
 			</div>
 		</div>
 	));
@@ -31,18 +37,6 @@ const Project = props => {
 				<h1 className="heading__primary">projects</h1>
 			</div>
 			<div className="projects">{cards}</div>
-			<input
-				type="file"
-				name="file"
-				onChange={props.uploadfileHandelr}
-				style={{ margin: "100px" }}
-			/>
-			<input
-				type="submit"
-				style={{ margin: "100px" }}
-				value="Submit"
-				onClick={props.uploadFile}
-			/>
 		</section>
 	);
 };
