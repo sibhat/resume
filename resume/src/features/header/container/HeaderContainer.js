61b1d837c7463884c2bc1e4e8a5f3776a9ae6470
sibhat <sibhat.temelso@gmail.com>
2019-05-01 19:27:20 -0700

    Header Container now reads from db

--- resume/src/features/header/container/HeaderContainer.js
+++ resume/src/features/header/container/HeaderContainer.js
@@ -4,20 +4,27 @@ import Header from "../presentational/Header";
 class HeaderContainer extends Component {
 	constructor(props) {
 		super(props);
-		this.state = { description: "", slogen: "", mission: "" };
+		this.state = {
+			about: {},
+			isLoading: true
+		};
 	}
 	componentDidMount() {
-		var starCountRef = firebase.database().ref("about");
-		starCountRef.on("value", snapshot => {
+		var aboutRef = firebase.database().ref("about");
+		aboutRef.on("value", snapshot => {
 			this.setState({
 				logoImg: snapshot.val().logoImg,
 				description: snapshot.val().description,
 				slogen: snapshot.val().slogen,
-				mission: snapshot.val().mission
+				mission: snapshot.val().mission,
+				isLoading: false
 			});
 		});
 	}
 	render() {
+		if(this.state.isLoading)
+			return <h1> waiting </h1>;
+
 		let { description, slogen, mission, logoImg } = this.state;
 		return (
 			<Header
@@ -25,6 +32,7 @@ class HeaderContainer extends Component {
 				slogen={slogen}
 				mission={mission}
 				logoImg={logoImg}
+				isLoading={this.state.isLoading}
 			/>
 		);
 	}
